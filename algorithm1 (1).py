0f611908bbb54bf7f52680aae07e3db97969fb82
Zahramohamed-eng <zahra05.mohamed@gmail.com>
2025-12-25 20:47:12 +0200

    Add files via upload

---
+++ algorithm1 (1).py
@@ -0,0 +1,8 @@
+def count_pairs_naive(arr, L, R):
+    count = 0
+    n = len(arr)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if L <= arr[i] + arr[j] <= R:
+                count += 1
+    return count
